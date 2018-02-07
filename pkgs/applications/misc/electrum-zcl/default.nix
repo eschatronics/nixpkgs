257fd33ba264bcffff97db9b0c1d8399c4d2ce3c
oblivia simplex <oblivia@paranoici.org>
2018-02-07 08:57:32 -0400

    first stab at electrum-zcl package

---
+++ pkgs/applications/misc/electrum-zcl/default.nix
@@ -0,0 +1,73 @@
+{ stdenv, fetchurl, python3, python3Packages, zbar }:
+
+python3Packages.buildPythonApplication rec {
+  name = "electrum-zclassic-${version}";
+  version = "1.0.3";
+
+  src = fetchurl {
+    url = "https://github.com/BTCP-community/electrum-zcl/archive/Z!${version}.tar.gz";
+    sha256 = "06z0a5p1jg93jialphslip8d72q9yg3651qqaf494gs3h9kw1sv1";
+  };
+
+  propagatedBuildInputs = with python3Packages; [
+    dnspython
+    ecdsa
+    jsonrpclib-pelix
+    matplotlib
+    pbkdf2
+    protobuf
+    pyaes
+    pycrypto
+    pyqt5
+    pysocks
+    qrcode
+    requests
+    tlslite
+
+    # plugins
+    keepkey
+    trezor
+
+    # TODO plugins
+    # amodem
+    # btchip
+  ];
+
+  preBuild = ''
+    sed -i 's,usr_share = .*,usr_share = "'$out'/share",g' setup.py
+    pyrcc5 icons.qrc -o gui/qt/icons_rc.py
+    # Recording the creation timestamps introduces indeterminism to the build
+    sed -i '/Created: .*/d' gui/qt/icons_rc.py
+    sed -i "s|name = 'libzbar.*'|name='${zbar}/lib/libzbar.so'|" lib/qrscanner.py
+  '';
+
+  postInstall = ''
+    # Despite setting usr_share above, these files are installed under
+    # $out/nix ...
+    mv $out/${python3.sitePackages}/nix/store"/"*/share $out
+    rm -rf $out/${python3.sitePackages}/nix
+
+    substituteInPlace $out/share/applications/electrum-zcl.desktop \
+      --replace "Exec=electrum-zcl %u" "Exec=$out/bin/electrum-zcl %u"
+  '';
+
+  doCheck = false;
+
+  doInstallCheck = true;
+  installCheckPhase = ''
+    $out/bin/electrum help >/dev/null
+  '';
+
+  meta = with stdenv.lib; {
+    description = "A lightweight ZClassic wallet";
+    longDescription = ''
+      An easy-to-use ZClassic client featuring wallets generated from
+      mnemonic seeds (in addition to other, more advanced, wallet options)
+      and the ability to perform transactions without downloading a copy
+      of the blockchain.
+    '';
+    homepage = "https://github.com/BTCP-community/electrum-zcl/";
+    license = licenses.mit;
+    maintainers = with maintainers; [ csulmone ];
+  };
+}
